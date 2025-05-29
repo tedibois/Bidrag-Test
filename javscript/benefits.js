8ae6e3106fadda6bdcc22c30f473641a58a91989
dib <60963856+tedibois@users.noreply.github.com>
2025-05-29 20:29:59 +0200

    fixed

--- javscript/benefits.js
+++ javscript/benefits.js
@@ -3,19 +3,38 @@ export const benefits = [
       name: "Aktivitetsersättning vid förlängd skolgång",
       url: "https://www.forsakringskassan.se/aktivitetsersattning",
       details: "Om du har en funktionsnedsättning som gör att du behöver längre tid för att bli klar med skolan, kan du få aktivitetsersättning vid förlängd skolgång.",
-      criteria: data => data.highSchool === "yes"  && data.disabled === "yes" && parseInt(data.age) >= 19 && parseInt(data.age) <= 29
+      criteria: data => 
+        data.highSchool === "yes"  &&
+       data.disabled === "yes" &&
+        parseInt(data.age) >= 19 &&
+         parseInt(data.age) <= 29
     },
     {
         name: "Aktivitetsersättning vid nedsatt arbetsförmåga",
         url: "https://www.forsakringskassan.se/privatperson/funktionsnedsattning/aktivitetsersattning/aktivitetsersattning-vid-nedsatt-arbetsformaga-for-dig-under-30-ar",
         details: "För dem som varit sjuka i ett eller flera år.",
-        criteria: data => data.inSchool === "yes" &&data.sick1Year === "yes" && data.disabled === "yes" && parseInt(data.age) >= 19 && parseInt(data.age) <= 29
+        criteria: data => data.inSchool === "yes" && 
+        data.sick1Year === "yes" && 
+        data.disabled === "yes" && 
+        parseInt(data.age) >= 19 &&
+        parseInt(data.age) <= 29
       },
+      {
+    name: "Sjukersättning (varaktig nedsättning)",
+    url: "https://www.forsakringskassan.se/privatperson/funktionsnedsattning/sjukersattning",
+    details:
+      "För dig 19-64 år som troligen aldrig kan arbeta heltid p.g.a. sjukdom/funktionsnedsättning.",
+    criteria: d =>
+      (d.sick === "yes" || d.disabled === "yes") &&
+      +d.age >= 19 &&
+      +d.age <= 64,
+  },
     {
       name: "Studiebidrag från CSN",
       url: "https://www.csn.se/bidrag-och-lan.html",
       details: "För dig som studerar.",
-      criteria: data => data.inSchool === "yes"
+      criteria: data => 
+      data.inSchool === "yes"
     },
     {
       name: "Bostadsbidrag för unga",
